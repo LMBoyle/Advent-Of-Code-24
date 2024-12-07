6a3e9f9796a6ca0922c1880f4bff94bd8af6ded5
LMBoyle <boylelukem@gmail.com>
2024-12-07 16:06:56 -0500

    set up folders to split up days. set up default files for days 2-7

--- days1-7/dayTwo/puzzleTwo.js
+++ days1-7/dayTwo/puzzleTwo.js
@@ -1,10 +1,29 @@
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 // DEPENDENCIES
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+const fs = require('fs');
+const csv = require('csv-parser');
 
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 // VARIABLES
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+// FUNCTION(S)
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+fs.createReadStream('dataInput.csv')
+.pipe(csv())
+.on('headers', (headers) => {
+    // DO SOMETHING WITH THE HEADERS/COLUMNS
+  })
+  .on('data', (row) => {
+    // DO SOMETHING WITH THE ROWS
+  })
+  .on('end', () => {
+    // DO SOMETHING AFTER DATA IS PROCESSED
+  })
 
 // x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x
 // END OF FILE
